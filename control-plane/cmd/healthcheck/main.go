48548f48b1fbd39b3082cdfd9e47546b5f903029
tallalmohar <tallalmoohar@gmail.com>
2026-08-29 17:17:37 -0700

    build: containerize scheduler and workers

---
+++ control-plane/cmd/healthcheck/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"time"
+
+	apollov1 "github.com/tm1944/Apollo-v2/control-plane/gen/apollo/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func main() {
+	address := flag.String("address", "localhost:50051", "control-plane address")
+	flag.Parse()
+	ctx, cancel := context.WithTimeout(context.Background(), 1500*time.Millisecond)
+	defer cancel()
+	conn, err := grpc.NewClient(*address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		os.Exit(1)
+	}
+	defer conn.Close()
+	if _, err = apollov1.NewJobServiceClient(conn).Health(ctx, &emptypb.Empty{}); err != nil {
+		os.Exit(1)
+	}
+}
